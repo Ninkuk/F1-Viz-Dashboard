5184cc37c57960d078cdcea0743890c2c8b3febd
ninkuk <ninadk03@gmail.com>
2022-10-15 20:14:16 -0700

    added basic map

--- js/map.js
+++ js/map.js
@@ -1,25 +1,28 @@
 // The svg
-const svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+const width = 1500;
+const height = 1000;
+
+const svg = d3.select("svg");
+svg.attr("width", width);
+svg.attr("height", height);
 
 // Map and projection
-const projection = d3.geoNaturalEarth1()
-    .scale(width / 1.3 / Math.PI)
-    .translate([width / 2, height / 2])
+const projection = d3
+  .geoMercator()
+  .scale(250)
+//   .center([0, 0])
+  .translate([width / 2, height / 2]);
+const pathGenerator = d3.geoPath().projection(projection);
 
-// Load external data and boot
-d3.json("data/world-map.json").then(function (data) {
-
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .join("path")
-        .attr("fill", "#69b3a2")
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        .style("stroke", "#fff")
-
-})
+d3.json("https://unpkg.com/world-atlas@2.0.2/countries-50m.json").then(
+  (data) => {
+    const countries = topojson.feature(data, data.objects.countries);
+    svg
+      .selectAll("path")
+      .data(countries.features)
+      .enter()
+      .append("path")
+      .attr("d", pathGenerator)
+      .attr("fill", "#00bfa5");
+  }
+);

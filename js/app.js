cec9b80efc31bf099af75d8144ad513b3a617da7
ninkuk <ninadk03@gmail.com>
2022-10-18 15:27:10 -0700

    added basic data fetch

--- js/app.js
+++ js/app.js
@@ -1,25 +1,62 @@
+// Year Range (Slider and Textfiels)
 const yearRange = document.getElementById("yearRange");
 const yearRangeText = document.getElementById("yearRangeText");
 
-yearRange.value = 2010;
-yearRangeText.value = 2010;
+// Get seasons list
+d3.csv("data/seasons.csv").then((data) => {
+  // sort years
+  const sortedSeasons = data.sort((a, b) => {
+    return a.year - b.year;
+  });
+
+  // Set min/max for year range
+  const min = sortedSeasons[0].year;
+  const max = sortedSeasons[sortedSeasons.length - 1].year;
+
+  yearRange.setAttribute("min", min);
+  yearRange.setAttribute("max", max);
+  yearRangeText.setAttribute("min", min);
+  yearRangeText.setAttribute("max", max);
+
+  yearRange.value = max - 10;
+  yearRangeText.value = max - 10;
+
+  renderViz(yearRange.value);
+});
 
 yearRange.addEventListener("change", (event) => {
   yearRangeText.value = event.target.value;
-  getData(yearRangeText.value);
+  getData(yearRange.value);
 });
 
 yearRangeText.addEventListener("change", (event) => {
   yearRange.value = event.target.value;
-  getData(yearRange.value);
+  getData(yearRangeText.value);
 });
 
-function getData(year) {
-  getSeasonRaceList(yearRange.value).then((races) => {
-    const circuits = [];
+function renderViz(year) {
+  const raceSchedule = getRaces(year);
+
+  const driverStandings = getDriverStandings(year);
+  const constructorStandings = getConstructorStandings(year);
+
+  const driverTotalPoints = driverStandings.then((drivers) =>
+    getDriverPoints(drivers)
+  );
+  const constructorTotalPoints = constructorStandings.then((constructors) =>
+    getConstructorPoints(constructors)
+  );
+
+  const countries = raceSchedule.then((races) => {
+    const countryList = [];
     races.forEach((race) => {
-      circuits.push(race.circuitId);
+      getRaceResults(year, race.round).then((data) => console.log(data));
+      countryList.push(race.Circuit.Location.country);
     });
-    getSeasonCircuitCoordinates(circuits);
+    return countryList;
   });
+
+  renderHeatmap(countries, driverStandings);
 }
+
+function getData(year) {}

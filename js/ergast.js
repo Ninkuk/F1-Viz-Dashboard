4f8f8f55c3ac92cf1c3b058e56ca6e1fd0345732
ninkuk <ninadk03@gmail.com>
2022-10-18 15:26:26 -0700

    switched to the api approach

--- js/ergast.js
+++ js/ergast.js
@@ -1,35 +1,55 @@
-const BASE_URL = "https://ergast.com/api/f1";
-const EXT = ".json";
-
-function getSeasonRaceList(year) {
-  return d3.csv("data/races.csv", (d) => {
-    if (d.year == year) {
-      return {
-        round: d.round,
-        name: d.name,
-        raceId: d.raceId,
-        circuitId: d.circuitId,
-      };
-    }
-  });
+function getRaces(year) {
+  return fetch(`https://ergast.com/api/f1/${year}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.MRData.RaceTable.Races;
+    });
 }
 
-function getSeasonCircuitCoordinates(circuits) {
-  d3.csv("data/circuits.csv", (d) => {
-    const ci = circuits.indexOf(d.circuitId);
-    if (ci != -1) {
-      circuits[ci] = {
-        circuitId: d.circuitId,
-        name: d.name,
-        location: d.location,
-        country: d.country,
-        lat: d.lat,
-        lng: d.lng,
-        alt: d.alt,
-      };
-    }
+function getRaceResults(year, round) {
+  return fetch(`https://ergast.com/api/f1/${year}/${round}/results.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.MRData.RaceTable.Races[0].Results;
+    });
+}
+
+function getDriverStandings(year) {
+  return fetch(`https://ergast.com/api/f1/${year}/driverStandings.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+}
+
+function getConstructorStandings(year) {
+  return fetch(`https://ergast.com/api/f1/${year}/constructorStandings.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    });
+}
+
+function getDriverPoints(drivers) {
+  const driverPoints = [];
+  drivers.forEach((driver) => {
+    driverPoints.push({
+      name: `${driver.Driver.givenName} ${driver.Driver.familyName}`,
+      points: driver.points,
+    });
   });
 
-  console.log(circuits);
-  return circuits;
+  return driverPoints;
+}
+
+function getConstructorPoints(constructors) {
+  const constructorPoints = [];
+  constructors.forEach((constructor) => {
+    constructorPoints.push({
+      name: constructor.Constructor.name,
+      points: constructor.points,
+    });
+  });
+
+  return constructorPoints;
 }
